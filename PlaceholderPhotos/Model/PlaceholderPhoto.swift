96537054ccbe819526c185a9bf64de62e04597ce
David Alarcon <momachilles@gmail.com>
2022-09-04 11:27:01 +0200

    Add PlaceholderPhoto.

---
+++ PlaceholderPhotos/Model/PlaceholderPhoto.swift
@@ -0,0 +1,15 @@
+//
+//  PlaceholderPhoto.swift
+//  PlaceholderPhotos
+//
+//  Created by David Alarcon on 4/9/22.
+//
+
+import Foundation
+
+struct PlaceholderPhoto: Codable {
+    /// INFO: We could also add 'albumId' and 'id' but we are not using them in this app.
+  let title: String
+  let url: String
+  let thumbnailUrl: String
+}
