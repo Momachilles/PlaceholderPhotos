0e7173b59964f95c66c585b73cf630bf754c1172
David Alarcon <momachilles@gmail.com>
2022-09-04 11:09:19 +0200

    Initial Commit

---
+++ PlaceholderPhotos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PlaceholderPhotos
+//
+//  Created by David Alarcon on 4/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
