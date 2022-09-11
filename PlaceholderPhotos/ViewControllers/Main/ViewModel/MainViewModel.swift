dbea4e5fdcf09e33f7783400cc50829ba055cb84
David Alarcon <momachilles@gmail.com>
2022-09-11 17:58:48 +0200

    Add main view model.

--- PlaceholderPhotos/ViewControllers/Main/ViewModel/MainViewModel.swift
+++ PlaceholderPhotos/ViewControllers/Main/ViewModel/MainViewModel.swift
@@ -5,8 +5,14 @@
 //  Created by David Alarcon on 11/9/22.
 //
 
-import UIKit
-
-class MainViewModel: NSObject {
-
+class MainViewModel {
+  var placeholderPhotos: [PlaceholderPhoto] = []
+  
+  func photos(completion: @escaping (() -> ())) throws {
+    try PhotosAPI().photos { [weak self] photos in
+      guard let self = self else { return }
+      self.placeholderPhotos = photos
+      completion()
+    }
+  }
 }
