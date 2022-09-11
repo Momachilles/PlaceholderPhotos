82958e4cb4d097d349e351c504c0f46a01921b3e
David Alarcon <momachilles@gmail.com>
2022-09-11 13:31:37 +0200

    Add custom placeholder photo cell.

---
+++ PlaceholderPhotos/ViewControllers/Main/Cell/View/PlaceholderPhotoTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  PlaceholderPhotoTableViewCell.swift
+//  PlaceholderPhotos
+//
+//  Created by David Alarcon on 11/9/22.
+//
+
+import UIKit
+
+class PlaceholderPhotoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
