30ccd57b61550e1b63e8967de030c68ce5f642a3
David Alarcon <momachilles@gmail.com>
2022-09-12 23:13:32 +0200

    Add activity indicator ro empty view.

--- PlaceholderPhotos/ViewControllers/Main/ViewModel/MainViewModel.swift
+++ PlaceholderPhotos/ViewControllers/Main/ViewModel/MainViewModel.swift
@@ -6,8 +6,13 @@
 //
 
 class MainViewModel {
+  weak var coordinator: MainCoordinator?
   var placeholderPhotos: [PlaceholderPhoto] = []
-  
+
+  init(coordinator: MainCoordinator) {
+    self.coordinator = coordinator
+  }
+
   func photos(completion: @escaping (() -> ())) throws {
     try PhotosAPI().photos { [weak self] photos in
       guard let self = self else { return }
