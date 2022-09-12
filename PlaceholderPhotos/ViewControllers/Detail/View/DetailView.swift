32e828e389c640a37910d6b8179471f4c3784250
David Alarcon <momachilles@gmail.com>
2022-09-12 23:07:11 +0200

    Add indicator view in detail image.

--- PlaceholderPhotos/ViewControllers/Detail/View/DetailView.swift
+++ PlaceholderPhotos/ViewControllers/Detail/View/DetailView.swift
@@ -10,6 +10,11 @@ import UIKit
 class DetailView: UIView {
   @IBOutlet weak var detailImageView: UIImageView!
   @IBOutlet weak var detailLabel: UILabel!
+  @IBOutlet weak var activityIndicatorView: UIActivityIndicatorView! {
+    didSet {
+      activityIndicatorView.startAnimating()
+    }
+  }
 
   var viewModel: DetailViewModel? {
     didSet {
@@ -17,6 +22,7 @@ class DetailView: UIView {
       detailLabel.text = viewModel?.text
       viewModel?.onDownloadedImage = { [weak self] in
         guard let self = self else { return }
+        self.activityIndicatorView.stopAnimating()
         UIView.transition(with: self.detailImageView, duration: 0.3, options: .transitionCrossDissolve) {
           self.detailImageView.image = self.viewModel?.image
         }
