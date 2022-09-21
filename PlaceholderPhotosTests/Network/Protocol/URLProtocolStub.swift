6d8eed76331c29ba5eb316e648860c4f6f4f51e6
David Alarcon <momachilles@gmail.com>
2022-09-21 12:42:22 +0200

    [5] Add missing URLProtocolStub.

--- PlaceholderPhotosTests/Network/Protocol/URLProtocolStub.swift
+++ PlaceholderPhotosTests/Network/Protocol/URLProtocolStub.swift
@@ -5,8 +5,30 @@
 //  Created by David Alarcon on 21/9/22.
 //
 
-import UIKit
+import XCTest
 
 class URLProtocolStub: URLProtocol {
-
+  static var handler: ((URLRequest) throws -> (HTTPURLResponse, Data))?
+  
+  override class func canInit(with request: URLRequest) -> Bool { true }
+  override class func canonicalRequest(for request: URLRequest) -> URLRequest { request }
+  
+  override func startLoading() {
+    guard let handler = Self.handler else {
+      XCTFail("Handler should be set.")
+      
+      return
+    }
+    
+    do {
+      let (response, data) = try handler(request)
+      client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
+      client?.urlProtocol(self, didLoad: data)
+      client?.urlProtocolDidFinishLoading(self)
+    } catch {
+      client?.urlProtocol(self, didFailWithError: error)
+    }
+  }
+  
+  override func stopLoading() {}
 }
